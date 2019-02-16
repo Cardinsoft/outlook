77c0f89359d3b324d66657428be62370a2340cda
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2019-02-16 03:42:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1751,7 +1751,7 @@ Switch.prototype.appendToUi = function (parent) {
 	}
 	
 	const label = document.createElement('label');
-	if(value==='on'||) {
+	if(value==='on') {
 		label.className = 'ms-Toggle-field is-selected';
 	}else {
 		label.className = 'ms-Toggle-field';
