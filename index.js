36dc987d4a4174f7ad97e5a594b49e1386d125dd
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2019-02-18 02:18:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1603,12 +1603,13 @@ function actionCallback(action,element) {
 		if(loadIndicator!=='NONE') {
 			const overlay = $('#app-overlay');
 			overlay.show();			
-		
+			/*
 			const indicator = document.createElement('div');
 			indicator.id = 'main-Ui-spinner';
 			indicator.className = 'ms-Spinner ms-Spinner--large';
 			overlay.append(indicator);
-			new fabric['Spinner'](indicator);	
+			new fabric['Spinner'](indicator);
+			*/	
 		}
 		
 		callbacks[functionName](parameters,element)
