3f4a5d7652292903d654503b4c61c0fcd095d716
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2019-02-18 23:14:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-
+console.log(new W_CardService());
   
 // The initialize function must be run each time a new page is loaded
 Office.initialize = (reason) => {
