7c33cf1ab4941b30cec67fff96265e2621a152f6
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2019-02-21 03:16:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2388,7 +2388,7 @@ KeyValue.prototype.appendToUi = function (parent,index) {
 	
 	//handle label and content creation;
 	const wrapText = document.createElement('div');
-	wrapText.className = 'column';
+	wrapText.className = 'column-text';
 	widget.append(wrapText);
 	
 	if(this.topLabel!==undefined) {	
@@ -2705,86 +2705,86 @@ function makeRequest(url,params) {
 
 //===========================================START GLOBALS===========================================//
 //headers;
-var globalOpenHeader = 'Welcome to Cardin';
-var globalInstallHeader = 'Connection setup';
-var globalSettingsHeader = 'Settings';
-var globalAdvancedHeader = 'Advanced';
-var globalHelpHeader = 'Help';
+var globalOpenHeader 			 = 'Welcome to Cardin';
+var globalInstallHeader 		 = 'Connection setup';
+var globalSettingsHeader 		 = 'Settings';
+var globalAdvancedHeader 		 = 'Advanced';
+var globalHelpHeader 			 = 'Help';
 var globalExistingConnectsHeader = 'Configured connections';
-var globalManualHeader = 'Manual fetch';
-var globalAutoHeader = 'Auto fetch';
-var globalCustomInstallHeader = 'Add new connection';
-var globalSuccessfulFetchHeader = 'Successfully fetched';
-var globalNoDataFetchHeader = 'Fetched with no data';
-var globalErroredHeader = 'Returned with error';
-var globalShowHeader = 'Contact';
-var globalUnparsedHeader = 'Unparsed data';
-var globalConfiguredHeader = 'Configured connections';
+var globalManualHeader 			 = 'Manual fetch';
+var globalAutoHeader 			 = 'Auto fetch';
+var globalCustomInstallHeader 	 = 'Add new connection';
+var globalSuccessfulFetchHeader  = 'Successfully fetched';
+var globalNoDataFetchHeader 	 = 'Fetched with no data';
+var globalErroredHeader 		 = 'Returned with error';
+var globalShowHeader 			 = 'Contact';
+var globalUnparsedHeader 		 = 'Unparsed data';
+var globalConfiguredHeader 		 = 'Configured connections';
 
 //titles;
-var globalWelcomeWidgetTitle = '';
-var globalInstallWidgetTitle = '';
-var globalCustomWidgetTitle = '';
-var globalCustomWidgetIconTitle = 'Card icon';
-var globalCustomWidgetNameTitle = 'Card name';
-var globalCustomWidgetInputTitle = 'Card URL';
-var globalResetWidgetTitle = 'Reset';
-var globalClearWidgetTitle = 'Cache';
-var globalCodeWidgetTitle = 'Code';
-var globalErrorWidgetTitle = 'Error info';
+var globalWelcomeWidgetTitle 	   = '';
+var globalInstallWidgetTitle 	   = '';
+var globalCustomWidgetTitle 	   = '';
+var globalCustomWidgetIconTitle    = 'Card icon';
+var globalCustomWidgetNameTitle    = 'Card name';
+var globalCustomWidgetInputTitle   = 'Card URL';
+var globalResetWidgetTitle 		   = 'Reset';
+var globalClearWidgetTitle 		   = 'Cache';
+var globalCodeWidgetTitle 		   = 'Code';
+var globalErrorWidgetTitle 		   = 'Error info';
 var globalUnparsedErrorWidgetTitle = 'Parse failure reason';
-var globalUnparsedDataWidgetTitle = 'Unparsed data';
-var globalNoDataWidgetTitle = '';
-var globalExtraDataTitle = '';
-var globalCustomWidgetFieldTitle = 'Field';
+var globalUnparsedDataWidgetTitle  = 'Unparsed data';
+var globalNoDataWidgetTitle 	   = '';
+var globalExtraDataTitle 		   = '';
+var globalCustomWidgetFieldTitle   = 'Field';
 
 //hints;
-var globalCustomWidgetIconHint = 'Card icon to display next to card name';
-var globalCustomWidgetNameHint = 'Card name to display in list of cards';
-var globalCustomWidgetHint = 'Card URL (eg Flow/Zapier Webhook trigger)';
+var globalCustomWidgetIconHint 	= 'Card icon to display next to card name';
+var globalCustomWidgetNameHint 	= 'Card name to display in list of cards';
+var globalCustomWidgetHint 		= 'Card URL (eg Flow/Zapier Webhook trigger)';
 var globalCustomWidgetFieldHint = 'Data to be sent with payload (for Zapier)';
 
 //contents & texts;
-var globalWelcomeWidgetContent = 'If you need help, setup instructions are available on resources below:';
-var globalCardinUrlText = 'cardinsoft.com';
-var globalYouTubeUrlText = 'YouTube instructions';
-var globalInstallWidgetSubmit = 'Move to display';
-var globalCustomWidgetContent = 'You can create custom connection by configuring form below. You can create as many connections as you want';
+var globalWelcomeWidgetContent 	 = 'If you need help, setup instructions are available on resources below:';
+var globalCardinUrlText 		 = 'cardinsoft.com';
+var globalYouTubeUrlText 		 = 'YouTube instructions';
+var globalInstallWidgetSubmit 	 = 'Move to display';
+var globalCustomWidgetContent 	 = 'You can create custom connection by configuring form below. You can create as many connections as you want';
 var globalCustomWidgetSubmitText = 'Add connection';
-var globalResetWidgetContent = 'Every user preference will be wiped clean, and all connections will be deleted';
-var globalClearWidgetContent = 'Cached data will be cleared, including information about successful connection fetches';
-var globalResetWidgetSubmitText = 'Reset';
-var globalClearWidgetSubmitText = 'Clear';
+var globalResetWidgetContent 	 = 'Every user preference will be wiped clean, and all connections will be deleted';
+var globalClearWidgetContent 	 = 'Cached data will be cleared, including information about successful connection fetches';
+var globalResetWidgetSubmitText  = 'Reset';
+var globalClearWidgetSubmitText  = 'Clear';
 var globalCustomWidgetSwitchText = 'Use manual data fetch';
-var globalRootText = 'Go back';
-var globalUpdateConnectionText = 'Update';
-var globalRemoveConnectionText = 'Remove connection';
-var globalNoDataWidgetText = 'The connection returned with no data to be displayed. If this is not an intended behaviour, please consider reconfiguring connection';
-var globalExtraDataText = 'There is more data to show, but we trimmed it to several sections to avoid hitting 100 widgets capping';
-var globalLoadExtraForwardText = 'Next';
-var globalLoadExtraBackText = 'Back';
-var globalSuccess = 'Success';
-var globalError = 'Error';
-var globalNoData = 'No data';
-var globalAuto = 'Auto';
-var globalManual = 'Manual';
-var globalGoToSettings = 'Go to settings';
+var globalRootText 				 = 'Go back';
+var globalUpdateConnectionText   = 'Update';
+var globalRemoveConnectionText   = 'Remove connection';
+var globalNoDataWidgetText 		 = 'The connection returned with no data to be displayed. If this is not an intended behaviour, please consider reconfiguring connection';
+var globalExtraDataText 		 = 'There is more data to show, but we trimmed it to several sections to avoid hitting 100 widgets capping';
+var globalLoadExtraForwardText 	 = 'Next';
+var globalLoadExtraBackText 	 = 'Back';
+var globalSuccess 				 = 'Success';
+var globalError 				 = 'Error';
+var globalNoData 				 = 'No data';
+var globalAuto 					 = 'Auto';
+var globalManual 				 = 'Manual';
+var globalGoToSettings 			 = 'Go to settings';
 
 //URLs;
-var globalCardinUrl = 'https://cardinsoft.com/';
+var globalCardinUrl  = 'https://cardinsoft.com/';
 var globalYouTubeUrl = 'https://www.youtube.com/';
 
 //notifications, warnings and error messages
-var globalUpdateSuccess = 'Configuration successfully updated!';
-var globalRemoveSuccess = 'Configuration successfully removed!';
-var globalClearSuccess  = 'Cache successfully cleared!';
-var globalIncorrectURL  = 'Please, consider reconfiguring connections listed below as their URLs returned with an error';
+var globalUpdateSuccess 	 = 'Configuration successfully updated!';
+var globalRemoveSuccess 	 = 'Configuration successfully removed!';
+var globalClearSuccess  	 = 'Cache successfully cleared!';
+var globalIncorrectURL  	 = 'Please, consider reconfiguring connections listed below as their URLs returned with an error';
 var globalInvalidURLnoMethod = 'URLs should include http or https method indication and domain';
 
 //other;
-var globalConfigName = 'config';
+var globalConfigName 	   = 'config';
 var globalNumUncollapsible = 5;
-var globalWidgetsCap = 50;
+var globalWidgetsCap       = 50;
 //===========================================END GLOBALS===========================================//
 
 //initiate services to be able to access them;
