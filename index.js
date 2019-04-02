26e0307e99caab716279d9887c3b294cb0c42659
Oleg Valter <oleg@cardinsoft.com>
2019-04-02 23:46:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6018,40 +6018,44 @@ class e_CardService {
 									REPLY_AS_DRAFT   : 'REPLY_AS_DRAFT',
 									STANDALONE_DRAFT : 'STANDALONE_DRAFT'
 								};
-		this.ContentType;
+		this.ContentType = {
+								TEXT           : 'TEXT',
+								MUTABLE_HTML   : 'MUTABLE_HTML',
+								IMMUTABLE_HTML : 'IMMUTABLE_HTML'
+							};
 		this.Icon = {
-			NONE 					 : '',	
-			AIRPLANE 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			BOOKMARK 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',	
-			BUS 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			CAR 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			CLOCK 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			CONFIRMATION_NUMBER_ICON : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			DOLLAR 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			DESCRIPTION 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			EMAIL 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			EVENT_PERFORMER 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			EVENT_SEAT 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			FLIGHT_ARRIVAL 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			FLIGHT_DEPARTURE 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			HOTEL 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			HOTEL_ROOM_TYPE 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			INVITE 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			MAP_PIN 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			MEMBERSHIP 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			MULTIPLE_PEOPLE 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			OFFER 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			PERSON 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			PHONE 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			RESTAURANT_ICON 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			SHOPPING_CART 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			STAR 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			STORE 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			TICKET 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			TRAIN 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			VIDEO_CAMERA 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
-			VIDEO_PLAY 				 : 'https://cardinsoft.github.io/outlook/assets/icons/'
-		};
+						NONE 					 : '',	
+						AIRPLANE 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						BOOKMARK 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',	
+						BUS 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						CAR 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						CLOCK 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						CONFIRMATION_NUMBER_ICON : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						DOLLAR 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						DESCRIPTION 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						EMAIL 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						EVENT_PERFORMER 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						EVENT_SEAT 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						FLIGHT_ARRIVAL 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						FLIGHT_DEPARTURE 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						HOTEL 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						HOTEL_ROOM_TYPE 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						INVITE 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						MAP_PIN 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						MEMBERSHIP 				 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						MULTIPLE_PEOPLE 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						OFFER 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						PERSON 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						PHONE 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						RESTAURANT_ICON 		 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						SHOPPING_CART 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						STAR 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						STORE 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						TICKET 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						TRAIN 					 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						VIDEO_CAMERA 			 : 'https://cardinsoft.github.io/outlook/assets/icons/',
+						VIDEO_PLAY 				 : 'https://cardinsoft.github.io/outlook/assets/icons/'
+					};
 		this.ImageStyle          = {
 										SQUARE : 'SQUARE',
 										CIRCLE : 'CIRCLE'
@@ -6182,7 +6186,7 @@ Properties.prototype.deleteProperty = function (key) {
 Properties.prototype.getProperty = function (key) {
 	const settings = this.settings;
 	let property = settings.get(key);
-	if(property!==undefined) { 
+	if(property) { 
 		return property; 
 	}else { 
 		return null; 
