cf8543049d48ca7e35462d23b2d716697b1afe56
Oleg Valter <oleg@cardinsoft.com>
2019-04-05 04:31:01 +0300

    Update GAS.js

--- GAS.js
+++ GAS.js
@@ -1407,7 +1407,6 @@ e_CacheService.prototype.getUserCache = function () {
 //initiate services to be able to access them;
 const CacheService      = new e_CacheService();
 const CardService       = new e_CardService();
-const e                 = new e_EventObject();
 const OAuth2			= new e_OAuth2();
 const PropertiesService = new e_PropertiesService();
 const UrlFetchApp       = new e_UrlFetchApp();
