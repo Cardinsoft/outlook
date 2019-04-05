d6bc918899c04192a1f530f91d01ac5cb4d3d137
Oleg Valter <oleg@cardinsoft.com>
2019-04-05 04:22:56 +0300

    Separated Flow+PD+Sheets

---
+++ Connectors/Flow.js
@@ -0,0 +1,51 @@
+//Flow connector class;
+function Flow() {
+  Connector.call(this);
+  this.icon  = globalFlowIconUrl;
+  this.name  = 'Flow';
+  this.short = globalFlowShort;
+  this.config = [
+    {
+      'header': 'Flow config',
+      'isCollapsible': false,
+      'widgets': [
+        {
+          'name': globalURLfieldName,
+          'type': 'TextInput',
+          'title': 'Flow URL',
+          'content': '',
+          'hint': 'e.g. Http get or post URL'
+        }
+      ]
+    }  
+  ];
+  this.auth = {};
+  this.run = function (msg,connector,data) {
+    //set method for url fetch ('get' or 'post' for now);
+    var method = 'post';
+
+    //set headers for url fetch;
+    var headers = {};
+    
+    //set payload in case POST request will be triggered;
+    var trimmed = trimMessage(msg,true,true);
+    var labels = []//msg.getThread().getLabels().map(function(label){ return label.getName(); });
+    var payload = {
+      'Bcc': msg.getBcc(),
+      'Cc': msg.getCc(),
+      'date': msg.getDate(),
+      'sender': trimmed.name,
+      'from': trimmed.email,
+      'id': msg.getId(),
+      'subject': msg.getSubject(),
+      'labels': labels
+    };
+    if(data) { payload.data = data; }
+    
+    //perform data fetch and return result;
+    var result = performFetch(connector.url,method,headers,payload);
+    return result;
+  }
+}
+//chain custom connector to base class;
+Flow.prototype = Object.create(Connector.prototype);
