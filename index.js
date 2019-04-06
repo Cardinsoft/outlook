e7343eb641a49d59defd59a97c564b32dd361320
Oleg Valter <oleg@cardinsoft.com>
2019-04-06 03:49:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4332,60 +4332,6 @@ class e_EventObject {
 }
 
 const e = new e_EventObject();
-
-//Emulate Message class that is obtained from current message auth flow;
-class Message {
-	constructor(msgFrom,msgBcc,msgCc,msgDate,msgPlainBody,msgSubject,msgId,msgThread) {
-		this.msgFrom      = msgFrom;
-		this.msgBcc       = msgBcc;
-		this.msgCc        = msgCc;
-		this.msgDate      = msgDate;
-		this.msgPlainBody = msgPlainBody;
-		this.msgSubject   = msgSubject;
-		this.msgId        = msgId;
-		this.msgThread    = msgThread;
-	}
-}
-Message.prototype.getId = function () {
-	return this.msgId;
-}
-Message.prototype.getBcc = function () {
-	return this.msgBcc;
-}
-Message.prototype.getCc = function () {
-	return this.msgCc;
-}
-Message.prototype.getDate = function () {
-	return this.msgDate;
-}
-Message.prototype.getFrom = function () {
-	return this.msgFrom;
-}
-Message.prototype.getPlainBody = function () {
-	return this.msgPlainBody;
-}
-Message.prototype.getSubject = function () {
-	return this.msgSubject;
-}
-Message.prototype.getThread = function () {
-	return this.msgThread;
-}
-
-/**
- * Fetches authorization token for current email
- * @param {Object} e -> event object;
- * @returns {Message}
- */
-function getToken(e) {
-	const item = Office.context.mailbox.item;
-	
-	const name = Office.context.mailbox.item.sender.displayName;
-	const email = Office.context.mailbox.item.sender.emailAddress;
-	const msgFrom = `${name} <${email}>`;
-	
-	const msg = new Message( msgFrom,'',item.cc,item.dateTimeCreated.toUTCString(),item.body,item.subject, item.itemId );
-	return msg;
-}
 //=========================================END GLOBAL OBJECTS=====================================//
 
 //===========================================START OAUTH2.0========================================//
@@ -4576,7 +4522,7 @@ function makeRequest(url,params) {
 		let request = new XMLHttpRequest();
 			request.timeout = 29000;
 			request.open(params.method.toUpperCase(),url);
-		if(params.contentType!==undefined) { request.setRequestHeader('Content-Type',params.contentType); }
+		if(params.contentType) { request.setRequestHeader('Content-Type',params.contentType); }
 			
 		request.onload = function () {
 			let status     = request.status;
