5afc50f99720fd56796ed6e395579dd57de4f74e
Oleg Valter <oleg@cardinsoft.com>
2019-04-08 01:57:51 +0300

    separated PropertiesService

--- GAS-JS/GAS.js
+++ GAS-JS/GAS.js
@@ -238,79 +238,6 @@ e_CardService.prototype.newUniversalActionResponseBuilder = function () {
 	return new UniversalActionResponseBuilder(); 
 }
 
-//Emulate PropertiesService service;
-class e_PropertiesService {
-	constructor() {
-		this.className = 'PropertiesService';
-	}
-}
-e_PropertiesService.prototype.getDocumentProperties = function () {
-	const settings = Office.context.roamingSettings;
-	return new Properties(settings,'document');	
-}
-e_PropertiesService.prototype.getScriptProperties = function () {
-	const settings = Office.context.roamingSettings;
-	return new Properties(settings,'script');	
-}
-e_PropertiesService.prototype.getUserProperties = function () {
-	const settings = Office.context.roamingSettings;
-	return new Properties(settings,'user');
-}
-
-//Emulate Class Properties for PropertiesService service;
-class Properties {
-	constructor(settings,type) {
-		this.settings = settings;
-		this.type     = type;
-	}
-}
-//add new methods to the class;
-Properties.prototype.deleteAllProperties = function () {
-	const settings = this.settings;
-	
-	console.log(Object.keys(typeof settings));
-}
-Properties.prototype.deleteProperty = function (key) {
-	let settings = this.settings;
-	settings.remove(key);
-	settings.saveAsync();
-	const type = this.type;
-	if(type==='user') { settings = Office.context.roamingSettings; }
-	const updated = new Properties(settings);
-	return updated;	
-}
-//Properties.prototype.getKeys = function () {} - not needed for initial release;
-//Properties.prototype.getProperties = function () {} - not needed for initial release;
-Properties.prototype.getProperty = function (key) {
-	const settings = this.settings;
-	let property = settings.get(key);
-	if(property) { 
-		return property; 
-	}else { 
-		return null; 
-	}
-}
-Properties.prototype.setProperties = function (properties,deleteAllOthers) { //add delete others after initial release;
-	const self = this;
-	for(let key in properties) {
-		let value = properties[key];
-		self.setProperty(key,value);
-	}
-	const type = this.type;
-	if(type==='user') { settings = Office.context.roamingSettings; }
-	const updated = new Properties(settings);
-	return updated;
-}
-Properties.prototype.setProperty = function (key,value) {
-	let settings = this.settings;
-	settings.set(key,value);
-	settings.saveAsync();
-	const type = this.type;
-	if(type==='user') { settings = Office.context.roamingSettings; }
-	const updated = new Properties(settings);
-	return updated;
-}
-
 //Emulate class Navigation for CardService service;
 class Navigation extends e_CardService {
 	constructor() {
@@ -1096,4 +1023,3 @@ Notification.prototype.appendToUi = function (/*parent*/) {
 
 //initiate services to be able to access them;
 const CardService       = new e_CardService();
-const PropertiesService = new e_PropertiesService();
