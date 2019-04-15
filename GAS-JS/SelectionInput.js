488a3e8b8b909e4a6a8e886bba6aff9abf983564
Oleg Valter <oleg@cardinsoft.com>
2019-04-15 03:59:02 +0300

    Update SelectionInput.js

--- GAS-JS/SelectionInput.js
+++ GAS-JS/SelectionInput.js
@@ -69,7 +69,11 @@ SelectionInput.prototype.appendToUi = function (parent) {
 	//SelectionInput Ui
 	switch(type) {
 		case 'CHECK_BOX':
-		
+				//set row;
+				widget = document.createElement('div');
+				widget.className = 'row '+className;
+				parent.append(widget);	
+				
 			//create inputs;
 			options.forEach(function(option){
 				//access option params;
@@ -77,10 +81,7 @@ SelectionInput.prototype.appendToUi = function (parent) {
 				let value    = option.value;
 				let checked  = option.selected;
 				
-				//set row;
-				widget = document.createElement('div');
-				widget.className = 'row '+className;
-				parent.append(widget);				
+			
 				
 				//set column;
 				row = document.createElement('div');
