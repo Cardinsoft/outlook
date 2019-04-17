1635c11831a849394143c2cbd1e5c5df034f8f05
Oleg Valter <oleg@cardinsoft.com>
2019-04-18 01:51:29 +0300

    Update CardSection.js

--- GAS-JS/CardSection.js
+++ GAS-JS/CardSection.js
@@ -94,7 +94,7 @@ CardSection.prototype.appendToUi = function (parent,serialize) {
 		toggler.className = 'toggler centered ms-Icon ms-Icon--ChevronDown pointer';
 		section.append(toggler);
 		
-		toggler.addEventListener('click',expand(toggler,widgetsWrap,1,5));
+		toggler.addEventListener('click',expand(toggler,widgetsWrap,4,1));
 		
 	}
 			
