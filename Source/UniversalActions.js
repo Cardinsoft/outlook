4fc1be621a68934528085c586356602871a04e78
Oleg Valter <oleg@cardinsoft.com>
2019-04-17 04:27:20 +0300

    -

--- Source/UniversalActions.js
+++ Source/UniversalActions.js
@@ -1,21 +1,23 @@
+/* 
+ * Attempts to clear any pre-cached data from the widget
+ * @param {Object} e event object;
+ */
 async function universalHome(e) {
-	/*return new Promise(
-		function(resolve) {*/
-			await cardOpen(e);
-		/*}
-	);*/
+	await cardOpen(e);
 };
+
+/**
+ * Opens settings card as corresponding universal action callback;
+ * @param {Object} e event object;
+ */
 async function universalSettings(e) {
-	/*return new Promise(
-		function(resolve) {*/
-			await cardSettings(e);
-	/*	}
-	);*/
+	await cardSettings(e);
 };
+
+/**
+ * Opens help card as corresponding universal action callback;
+ * @param {Object} e event object;
+ */
 async function universalHelp(e) {
-	/*return new Promise(
-		function(resolve) {*/
-			await cardHelp(e);
-		/*}
-	);*/
+	await cardHelp(e);
 };
