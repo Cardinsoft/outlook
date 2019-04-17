0bad1083fe6b8ad98aa6b82a6b35c91bd0468cb2
Oleg Valter <oleg@cardinsoft.com>
2019-04-18 02:48:52 +0300

    Update CardSection.js

--- GAS-JS/CardSection.js
+++ GAS-JS/CardSection.js
@@ -96,7 +96,7 @@ CardSection.prototype.appendToUi = function (parent,serialize) {
 		
 		wrapper.style.height = 0;
 	
-		toggler.addEventListener('click',expand(toggler,wrapper,1,1));
+		toggler.addEventListener('click',expand(toggler,wrapper,1,6));
 		
 	}
 			
