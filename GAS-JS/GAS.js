152969ed84800abefe0c388d65140fcda67b4dc5
Oleg Valter <oleg@cardinsoft.com>
2019-04-19 04:35:20 +0300

    Update GAS.js

--- GAS-JS/GAS.js
+++ GAS-JS/GAS.js
@@ -228,7 +228,7 @@ CardBuilder.prototype.build = async function () {
 	
 	if(this.cardHeader) {
 		const headerWrap = document.createElement('div');
-		headerWrap.className = [cardHeader.className,'separated-both'].join(' ');
+		headerWrap.className = [cardHeader.className,'separated'].join(' ');
 		$('#app-body').prepend(headerWrap);
 		
 		if(this.cardHeader.imageUrl) {
