63110fb05b426c1af31187362990a2826b25500f
Oleg Valter <oleg@cardinsoft.com>
2019-04-27 02:28:08 +0300

    splitted Action

--- GAS-JS/GAS.js
+++ GAS-JS/GAS.js
@@ -292,53 +292,6 @@ CardHeader.prototype.setSubtitle = function (subtitle) {
 	return this;
 };
 
-//Emulate Class Action for CardService service;
-class Action {
-	constructor() {
-		this.className = 'Action';
-		this.functionName;
-		this.loadIndicator;
-		this.parameters;
-	}
-}
-//add new methods to the class;
-Action.prototype.setFunctionName = function (functionName) {
-	this.functionName = functionName;
-	return this;
-}
-Action.prototype.setLoadIndicator = function (loadIndicator) {
-	this.loadIndicator = loadIndicator;
-	return this;
-}
-Action.prototype.setParameters = function (parameters) {
-	this.parameters = parameters;
-	return this;
-}
-
-//Emulate Class OpenLink extending Class Action for CardService service;
-class OpenLink extends Action {
-	constructor() {
-		super();
-		this.className = 'OpenLink';
-		this.url;
-		this.onClose;
-		this.openAs;
-	}
-}
-//add new methods to the class;
-OpenLink.prototype.setUrl = function (url) {
-	this.url = url;
-	return this;	
-}
-OpenLink.prototype.setOnClose = function (onClose) {
-	this.onClose = onClose;
-	return this;
-}
-OpenLink.prototype.setOpenAs = function (openAs) {
-	this.openAs = openAs;
-	return this;
-}
-
 //Emulates class AuthorizationAction extending Action for CardService service;
 class AuthorizationAction extends e_CardService {
 	constructor() {
