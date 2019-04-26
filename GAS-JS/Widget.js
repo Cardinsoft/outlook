a4c9f6a12f90572f903c642e07e7e3ff9068ca79
Oleg Valter <oleg@cardinsoft.com>
2019-04-27 02:53:07 +0300

    splitted CardService

--- GAS-JS/Widget.js
+++ GAS-JS/Widget.js
@@ -35,3 +35,42 @@ TextParagraph.prototype.appendToUi = function(parent) {
 	content.innerHTML = this.content;
 	wrapText.append(content);
 }
+
+//Emulate Class ButtonSet extending base Class Widget for CardService service;
+class ButtonSet extends Widget {
+	constructor() {
+		super();
+		this.className = 'ButtonSet';
+		this.buttons = [];
+	}
+}
+//add new methods to the class;
+ButtonSet.prototype.addButton = function(button) {
+	this.buttons.push(button);
+	return this;
+}
+ButtonSet.prototype.appendToUi = function(parent) {
+	const buttons = this.buttons;
+	const length = buttons.length;
+	
+	const btnRow = document.createElement('div');
+	btnRow.className = 'row '+this.className;
+	parent.append(btnRow);
+	
+	const wrapBtn = document.createElement('div');
+	wrapBtn.className = 'column';
+	btnRow.append(wrapBtn);
+	
+	buttons.forEach(function(button) {
+		const backgroundColor = button.backgroundColor;
+		const text            = button.text;
+		const disabled        = button.disabled;
+		const textButtonStyle = button.textButtonStyle;	
+		const action          = button.action;
+		const openLink        = button.openLink;
+		const authAction      = button.authorizationAction; 
+		
+		button.appendToUi(wrapBtn);
+	});
+
+}
