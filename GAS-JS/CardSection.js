a23e494c6a7980411b8cb8c4f923b59de8faab09
Oleg Valter <oleg@cardinsoft.com>
2019-04-27 00:35:00 +0300

    support for non-0 initial values

--- GAS-JS/CardSection.js
+++ GAS-JS/CardSection.js
@@ -119,7 +119,7 @@ CardSection.prototype.appendToUi = async function (parent,serialize,sI) {
 		wrapper.style.height = initial+'px';
 		
 		//add event handler for toggling collapsed state;
-		toggler.addEventListener('click',collapse(toggler,wrapper,'height',1,2));
+		toggler.addEventListener('click',collapse(toggler,wrapper,'height',1,2,initial));
 		
 		//add event handler for toggling target element's state;
 		toggler.addEventListener('click',function(){
