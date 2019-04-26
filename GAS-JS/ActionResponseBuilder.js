5c2b072b9aee2862644497f7ef47d5763b2d3ff9
Oleg Valter <oleg@cardinsoft.com>
2019-04-27 02:32:09 +0300

    splitted ActionResponseBuilder

---
+++ GAS-JS/ActionResponseBuilder.js
@@ -0,0 +1,35 @@
+//Emulate Class ActionResponseBuilder for CardService service;
+class ActionResponseBuilder {
+	constructor() {
+		super();
+		this.navigation;
+		this.notification;
+		this.openLink;
+		this.stateChanged = false;
+	}
+}
+//add new methods to the class;
+ActionResponseBuilder.prototype.setNavigation = function (navigation) {
+	this.navigation = navigation;
+	return this;	
+}
+ActionResponseBuilder.prototype.setNotification = function (notification) {
+	this.notification = notification;
+	return this;	
+}
+ActionResponseBuilder.prototype.setOpenLink = function (openLink) {
+	this.openLink = openLink;
+	return this;	
+}
+ActionResponseBuilder.prototype.setStateChanged = function (stateChanged) {
+	this.stateChanged = stateChanged;
+	return this;	
+}
+ActionResponseBuilder.prototype.build = function () {
+	const notif = this.notification;
+	if(notif) {
+		const ui = $('#main-Ui-wrap');
+		notif.appendToUi(ui);
+		return this;
+	}
+}
--- GAS-JS/GAS.js
+++ GAS-JS/GAS.js
@@ -292,42 +292,5 @@ CardHeader.prototype.setSubtitle = function (subtitle) {
 	return this;
 };
 
-//Emulate Class ActionResponseBuilder extending _CardService service;
-class ActionResponseBuilder extends e_CardService {
-	constructor() {
-		super();
-		this.navigation;
-		this.notification;
-		this.openLink;
-		this.stateChanged = false;
-	}
-}
-//add new methods to the class;
-ActionResponseBuilder.prototype.setNavigation = function (navigation) {
-	this.navigation = navigation;
-	return this;	
-}
-ActionResponseBuilder.prototype.setNotification = function (notification) {
-	this.notification = notification;
-	return this;	
-}
-ActionResponseBuilder.prototype.setOpenLink = function (openLink) {
-	this.openLink = openLink;
-	return this;	
-}
-ActionResponseBuilder.prototype.setStateChanged = function (stateChanged) {
-	this.stateChanged = stateChanged;
-	return this;	
-}
-ActionResponseBuilder.prototype.build = function () {
-	const notif = this.notification;
-	if(notif) {
-		const ui = $('#main-Ui-wrap');
-		notif.appendToUi(ui);
-		return this;
-	}
-}
-
-
 //initiate services to be able to access them;
 const CardService = new e_CardService();
