672dbb77a4affb24215740323c763d23a223900e
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2019-05-02 11:33:19 +0300

    Update CardBuilder.js

--- GAS-JS/CardBuilder.js
+++ GAS-JS/CardBuilder.js
@@ -111,7 +111,7 @@ CardBuilder.prototype.build = async function () {
 			if(menu.menu.children.length>3) { menu.removeItem(0); } //make adjustable in future updates;
 			menu.addItem(item,true);
 		}else {
-			console.log(menus);
+			console.log(menus[0].menu.children.length>3);
 		}
 		
 
