5da9fcf45ea642697eacd0f1d0924eaae2b8a0de
Oleg Valter <oleg@cardinsoft.com>
2019-06-19 01:36:42 +0300

    -

---
+++ GAS-JS/Utilities.js
@@ -0,0 +1,26 @@
+/**
+ * Constructor function for Utilities service;
+ */
+function e_Utilities() {
+	this.className = 'Utilities';
+}
+
+/**
+ * Encodes input into base-64 String;
+ * @returns {String} input;
+ */
+e_Utilities.prototype.base64Encode = function (input) {
+	let encoded;
+	encoded = btoa(input);
+	return encoded;
+}
+
+/**
+ * Decodes input from base-64 String;
+ * @returns {String} input;
+ */
+e_Utilities.prototype.base64Decode = function (encoded) {
+	let decoded;
+	decoded = atob(encoded);
+	return decoded;
+}
