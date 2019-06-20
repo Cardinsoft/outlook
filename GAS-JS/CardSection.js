6f0b18ef48b4a376d4dc5ffdcbfd1be9d4877176
Oleg Valter <oleg@cardinsoft.com>
2019-06-20 05:46:27 +0300

    -

--- GAS-JS/CardSection.js
+++ GAS-JS/CardSection.js
@@ -60,8 +60,6 @@ CardSection.prototype.appendToUi = async function (parent,serialize,sI) {
 	//access widgets and append;
 	const widgets = this.widgets;
 	
-	console.log(widgets)
-	
 	if(widgets.length!==0) {
 
 		//check if at least one widget is a form input;
