7500a96bed7062813cce614f5da76336a7d72cb0
Oleg Valter <oleg@cardinsoft.com>
2019-06-22 07:03:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -929,7 +929,7 @@ DOMTokenList.prototype.add = function () {
 	console.log(arguments);
 	
 	for(let arg = 0; arg < arguments.length; arg++) {
-		DOMTokenList.push(arguments[arg]);
+		this.add(arguments[arg]);
 	}
 	
 }
