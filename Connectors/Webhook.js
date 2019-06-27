ecbf8dca5a009315a473401812349d789ccaf82c
Oleg Valter <oleg@cardinsoft.com>
2019-06-27 03:17:37 +0300

    Update Webhook.js

--- Connectors/Webhook.js
+++ Connectors/Webhook.js
@@ -1,40 +1,8 @@
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-  try {
-    var info = gen[key](arg);
-    var value = info.value;
-  } catch (error) {
-    reject(error);
-    return;
-  }
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-  if (info.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(_next, _throw);
-  }
-}
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-function _asyncToGenerator(fn) {
-  return function () {
-    var self = this,
-        args = arguments;
-    return new Promise(function (resolve, reject) {
-      var gen = fn.apply(self, args);
-
-      function _next(value) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-      }
-
-      function _throw(err) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-      }
-
-      _next(undefined);
-    });
-  };
-} //Flow connector class;
-
-
+//Flow connector class;
 function Flow() {
   Connector.call(this);
   this.icon = globalIconWebhook;
@@ -60,7 +28,7 @@ function Flow() {
     var _ref = _asyncToGenerator(
     /*#__PURE__*/
     regeneratorRuntime.mark(function _callee(msg, connector, data) {
-      var method, headers, trimmed, payload, result;
+      var method, headers, trimmed, payload, result, content, returned;
       return regeneratorRuntime.wrap(function _callee$(_context) {
         while (1) switch (_context.prev = _context.next) {
           case 0:
@@ -71,7 +39,6 @@ function Flow() {
             connector.method = 'send'; //set payload in case POST request will be triggered;
 
             trimmed = trimMessage(msg, true, true);
-			
             payload = {
               'Bcc': msg.getBcc(),
               'Cc': msg.getCc(),
@@ -81,7 +48,7 @@ function Flow() {
               'id': msg.getId(),
               'subject': msg.getSubject()
             };
-			
+
             if (data) {
               payload.data = data;
             } //perform data fetch and return result;
@@ -92,9 +59,31 @@ function Flow() {
 
           case 8:
             result = _context.sent;
-            return _context.abrupt("return", result);
+            content = result.content;
+            returned = {
+              code: result.code,
+              headers: result.headers,
+              content: content,
+              hasMatch: {
+                value: false,
+                text: 'found'
+              }
+            };
 
-          case 10:
+            try {
+              content = JSON.parse(content);
+
+              if (content.hasMatch) {
+                returned.content = content.content;
+                returned.hasMatch = content.hasMatch;
+              }
+            } catch (e) {
+              console.log('Webhook Connector tried to parse response and failed - this error is usually harmless');
+            }
+
+            return _context.abrupt("return", returned);
+
+          case 13:
           case "end":
             return _context.stop();
         }
