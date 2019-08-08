fc2a9882b0d90bd4b9a36570d454f6b8cf73c45b
Oleg Valter <oleg@cardinsoft.com>
2019-08-09 01:31:59 +0300

    Update Switch.js

--- GAS-JS/Switch.js
+++ GAS-JS/Switch.js
@@ -1,93 +1,20 @@
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-  try {
-    var info = gen[key](arg);
-    var value = info.value;
-  } catch (error) {
-    reject(error);
-    return;
-  }
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-  if (info.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(_next, _throw);
-  }
-}
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-function _asyncToGenerator(fn) {
-  return function () {
-    var self = this,
-        args = arguments;
-    return new Promise(function (resolve, reject) {
-      var gen = fn.apply(self, args);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-      function _next(value) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-      }
+function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-      function _throw(err) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-      }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
-      _next(undefined);
-    });
-  };
-}
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
-function _possibleConstructorReturn(self, call) {
-  if (call && (typeof call === "object" || typeof call === "function")) {
-    return call;
-  }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-  return _assertThisInitialized(self);
-}
-
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-  }
-
-  return self;
-}
-
-function _getPrototypeOf(o) {
-  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
-    return o.__proto__ || Object.getPrototypeOf(o);
-  };
-  return _getPrototypeOf(o);
-}
-
-function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function");
-  }
-
-  subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: {
-      value: subClass,
-      writable: true,
-      configurable: true
-    }
-  });
-  if (superClass) _setPrototypeOf(subClass, superClass);
-}
-
-function _setPrototypeOf(o, p) {
-  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
-    o.__proto__ = p;
-    return o;
-  };
-
-  return _setPrototypeOf(o, p);
-} //Emulate Class Switch for CardService service;
-
-
+//Emulate Class Switch for CardService service;
 let Switch =
 /*#__PURE__*/
 function (_e_CardService) {
@@ -125,25 +52,33 @@ Switch.prototype.setSelected = function (selected) {
   this.selected = selected;
   return this;
 };
+/**
+ * Sets value passed to form;
+ * @param {*} value value to pass to form;
+ * @returns {Switch} this switch;
+ */
+
 
 Switch.prototype.setValue = function (value) {
   this.value = value;
   return this;
 };
+/**
+ * Appends widget to Ui;
+ * @parent {HtmlElement} parent element to append widget to;
+ */
+
 
 Switch.prototype.appendToUi = function (parent) {
   //access Switch parameters;
   const fieldName = this.fieldName;
   let action = this.action;
   const selected = this.selected;
-  const value = this.value; //create toggler paragraph;
-
-  const pToggle = document.createElement('p');
-  parent.appendChild(pToggle); //create toggler wrapper and set required parameters;
+  const value = this.value; //create toggler wrapper and set required parameters;
 
   const wrapToggle = document.createElement('div');
   wrapToggle.className = 'ms-Toggle ms-font-m-plus ' + this.className;
-  pToggle.appendChild(wrapToggle); //create input and set required parameters;
+  parent.append(wrapToggle); //create input and set required parameters;
 
   const input = document.createElement('input');
   input.id = fieldName;
@@ -152,21 +87,21 @@ Switch.prototype.appendToUi = function (parent) {
   input.name = fieldName;
   input.value = value; //append toggler wrap;
 
-  wrapToggle.appendChild(input); //set action if provided;
+  wrapToggle.append(input); //set action if provided;
 
   if (action) {
     //parse action if found;
     action = JSON.parse(action); //change cursor to pointer on hover;
 
-    widget.classList.add('pointer'); //get unique identifier;
+    wrapToggle.classList.add('pointer'); //get unique identifier;
 
     let id = getId(); //set stringifyed action to global storage;
 
     e_actions[id] = JSON.stringify(action); //add action reference to widget;
 
-    widget.setAttribute('action', id); //set event listener to widget;
+    wrapToggle.setAttribute('action', id); //set event listener to widget;
 
-    widget.addEventListener('click',
+    wrapToggle.addEventListener('click',
     /*#__PURE__*/
     _asyncToGenerator(
     /*#__PURE__*/
@@ -200,6 +135,6 @@ Switch.prototype.appendToUi = function (parent) {
     input.classList.toggle('is-selected');
   }); //append toggle label to wrapper;
 
-  wrapToggle.appendChild(label);
+  wrapToggle.append(label);
   new fabric['Toggle'](wrapToggle);
 };
