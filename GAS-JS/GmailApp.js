48d42c4444da476911ad6c8794ab219a09ef0a8f
Oleg Valter <oleg@cardinsoft.com>
2019-08-18 17:36:03 +0300

    Update GmailApp.js

--- GAS-JS/GmailApp.js
+++ GAS-JS/GmailApp.js
@@ -1,10 +1,6 @@
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-} //Emulate GmailApp service (partially);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-
+//Emulate GmailApp service (partially);
 let e_GmailApp = function e_GmailApp() {
   _classCallCheck(this, e_GmailApp);
 
@@ -20,16 +16,20 @@ e_GmailApp.prototype.setCurrentMessageAccessToken = function (accessToken) {};
 /**
  * Gets message by its Id;
  * @param {String} messageId message Id to lookup;
+ * @return {Object} Message class instance;
  */
 
 
 e_GmailApp.prototype.getMessageById = function (messageId) {
   const item = Office.context.mailbox.item;
-  const name = Office.context.mailbox.item.sender.displayName;
-  const email = Office.context.mailbox.item.sender.emailAddress;
-  const msgFrom = "".concat(name, " <").concat(email, ">");
-  const msg = new Message(msgFrom, '', item.cc, item.dateTimeCreated.toUTCString(), item.body, item.normalizedSubject, item.itemId, item);
-  return msg;
+
+  if (item !== null) {
+    const name = Office.context.mailbox.item.sender.displayName;
+    const email = Office.context.mailbox.item.sender.emailAddress;
+    const msgFrom = "".concat(name, " <").concat(email, ">");
+    const msg = new Message(msgFrom, '', item.cc, item.dateTimeCreated.toUTCString(), item.body, item.normalizedSubject, item.itemId, item);
+    return msg;
+  }
 }; //initiate GmailApp service;
 
 
