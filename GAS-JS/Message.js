739ab829ba5579f244a0302fc9ba7d58aec5a806
Oleg Valter <oleg@cardinsoft.com>
2019-08-31 06:43:22 +0300

    Update Message.js

--- GAS-JS/Message.js
+++ GAS-JS/Message.js
@@ -1,86 +1,80 @@
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-  try {
-    var info = gen[key](arg);
-    var value = info.value;
-  } catch (error) {
-    reject(error);
-    return;
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+//Emulate Message class that is obtained from current message auth flow;
+let Message =
+/*#__PURE__*/
+function () {
+  function Message(msg) {
+    _classCallCheck(this, Message);
+
+    this.id = msg.id;
+    this.from = msg.from;
+    this.to = msg.to;
+    this.subject = msg.subject || '';
+    this.bcc = msg.bcc || [];
+    this.cc = msg.cc || [];
+    this.plain = msg.plain || '';
+    this.date = new Date(msg.date);
+    this.thread = msg.thread;
   }
 
-  if (info.done) {
-    resolve(value);
-  } else {
-    Promise.resolve(value).then(_next, _throw);
-  }
-}
+  _createClass(Message, [{
+    key: "getId",
+    value: function getId() {
+      return this.id;
+    }
+  }, {
+    key: "getFrom",
+    value: function getFrom() {
+      return this.from;
+    }
+  }, {
+    key: "getTo",
+    value: function getTo() {
+      return this.to;
+    }
+  }, {
+    key: "getSubject",
+    value: function getSubject() {
+      return this.subject;
+    }
+  }, {
+    key: "getCc",
+    value: function getCc() {
+      return this.cc.join(',');
+    }
+  }, {
+    key: "getBcc",
+    value: function getBcc() {
+      return this.bcc.join(',');
+    }
+  }, {
+    key: "getDate",
+    value: function getDate() {
+      return this.date;
+    }
+  }, {
+    key: "getPlainBody",
+    value: function getPlainBody() {
+      return this.plain;
+    }
+  }, {
+    key: "getThread",
+    value: function getThread() {
+      return new Thread(this.thread);
+    }
+  }]);
 
-function _asyncToGenerator(fn) {
-  return function () {
-    var self = this,
-        args = arguments;
-    return new Promise(function (resolve, reject) {
-      var gen = fn.apply(self, args);
-
-      function _next(value) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-      }
-
-      function _throw(err) {
-        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-      }
-
-      _next(undefined);
-    });
-  };
-}
-
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-} //Emulate Message class that is obtained from current message auth flow;
-
-
-let Message = function Message(msgFrom, msgBcc, msgCc, msgDate, msgPlainBody, msgSubject, msgId, msgThread) {
-  _classCallCheck(this, Message);
-
-  this.msgFrom = msgFrom;
-  this.msgBcc = msgBcc;
-  this.msgCc = msgCc;
-  this.msgDate = msgDate;
-  this.msgPlainBody = msgPlainBody;
-  this.msgSubject = msgSubject;
-  this.msgId = msgId;
-  this.msgThread = msgThread;
-};
-
-Message.prototype.getId = function () {
-  return this.msgId;
-};
-
-Message.prototype.getBcc = function () {
-  return this.msgBcc;
-};
-
-Message.prototype.getCc = function () {
-  return this.msgCc;
-};
-
-Message.prototype.getDate = function () {
-  return this.msgDate;
-};
-
-Message.prototype.getFrom = function () {
-  return this.msgFrom;
-};
-
-Message.prototype.getPlainBody = function () {
-  return this.msgPlainBody;
-};
-
-Message.prototype.getSubject = function () {
-  return this.msgSubject;
-};
+  return Message;
+}();
 
 Message.prototype.getThread = function () {
   return new Thread(this.msgThread);
