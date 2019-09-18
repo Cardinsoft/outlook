df3dbe7b65da6b77f3ba37577941d2e4d1b7c828
Oleg Valter <oleg@cardinsoft.com>
2019-09-18 05:18:06 +0300

    Update Utilities.js

--- GAS-JS/Utilities.js
+++ GAS-JS/Utilities.js
@@ -10,6 +10,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 function sleepy(ms) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
+      console.log('awoke');
       resolve(true);
     }, ms);
   });
