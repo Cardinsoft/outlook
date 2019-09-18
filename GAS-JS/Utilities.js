7d2aaa0c2a9b43d48fc3b03e371607691b2a5005
Oleg Valter <oleg@cardinsoft.com>
2019-09-18 05:12:27 +0300

    Update Utilities.js

--- GAS-JS/Utilities.js
+++ GAS-JS/Utilities.js
@@ -1,9 +1,58 @@
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+/**
+ * Sets timeout to sleep;
+ * @param {Integer} ms milliseconds to wait;
+ * @return {Object} resolved Promise;
+ */
+function sleepy(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(true);
+    }, ms);
+  });
+}
 /**
  * Constructor function for Utilities service;
  */
+
+
 function e_Utilities() {
   this.className = 'Utilities';
 }
+/**
+ * Sleeps for specified number of ms;
+ * @param {Integer} milliseconds number of ms to wait;
+ */
+
+
+e_Utilities.prototype.sleep =
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee(milliseconds) {
+    var ms;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) switch (_context.prev = _context.next) {
+        case 0:
+          ms = (ms <= 300000 ? ms : 300000) || 0;
+          _context.next = 3;
+          return sleepy(ms);
+
+        case 3:
+        case "end":
+          return _context.stop();
+      }
+    }, _callee);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
 /**
  * Encodes input into base-64 String;
  * @returns {String} input;
