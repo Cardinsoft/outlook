c27e7a83c7b386b2d9b118f2c30378f6d0ca690d
Oleg Valter <oleg@cardinsoft.com>
2019-09-18 05:25:12 +0300

    Update Close.js

--- Connectors/Close.js
+++ Connectors/Close.js
@@ -178,19 +178,14 @@ function Close() {
           case 11:
             removeResponse = _context2.sent;
 
-            if (!(removeResponse.code >= 200 && removeResponse.code < 300)) {
-              _context2.next = 16;
-              break;
+            if (removeResponse.code >= 200 && removeResponse.code < 300) {
+              content = JSON.parse(removeResponse.content);
+              Utilities.sleep(500);
             }
 
-            content = JSON.parse(removeResponse.content);
-            _context2.next = 16;
-            return Utilities.sleep(500);
-
-          case 16:
             return _context2.abrupt("return", this.run(msg, connector));
 
-          case 17:
+          case 14:
           case "end":
             return _context2.stop();
         }
