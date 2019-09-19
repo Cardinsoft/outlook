47c989bf2c9eff2eba86b5aa5c55bfc29f3bb087
Oleg Valter <oleg@cardinsoft.com>
2019-09-19 06:37:20 +0300

    Update UniversalActionResponseBuilder.js

--- GAS-JS/UniversalActionResponseBuilder.js
+++ GAS-JS/UniversalActionResponseBuilder.js
@@ -74,7 +74,7 @@ function () {
     key: "build",
     value: function build() {
       const response = new UniversalActionResponse(this);
-      return response.returns; //TODO: refactor;
+      return response; //TODO: refactor;
     }
   }]);
 
