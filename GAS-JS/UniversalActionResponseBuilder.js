318412875eab7ca39e9e5172019710620fad6de8
Oleg Valter <oleg@cardinsoft.com>
2019-09-19 06:55:53 +0300

    Update UniversalActionResponseBuilder.js

--- GAS-JS/UniversalActionResponseBuilder.js
+++ GAS-JS/UniversalActionResponseBuilder.js
@@ -29,20 +29,38 @@ function () {
     value: function () {
       var _displayAddOnCards = _asyncToGenerator(
       /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee(cardObjects) {
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) switch (_context.prev = _context.next) {
+      regeneratorRuntime.mark(function _callee2(cardObjects) {
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) switch (_context2.prev = _context2.next) {
             case 0:
-              console.log(cardObjects);
-              /*cardObjects.forEach(async function(cardObject){
-              	await cardObject.appendToUi();
-              });*/
+              cardObjects.forEach(
+              /*#__PURE__*/
+              function () {
+                var _ref = _asyncToGenerator(
+                /*#__PURE__*/
+                regeneratorRuntime.mark(function _callee(cardObject) {
+                  return regeneratorRuntime.wrap(function _callee$(_context) {
+                    while (1) switch (_context.prev = _context.next) {
+                      case 0:
+                        console.log(cardObject.appendToUi);
+
+                      case 1:
+                      case "end":
+                        return _context.stop();
+                    }
+                  }, _callee);
+                }));
+
+                return function (_x2) {
+                  return _ref.apply(this, arguments);
+                };
+              }());
 
             case 1:
             case "end":
-              return _context.stop();
+              return _context2.stop();
           }
-        }, _callee);
+        }, _callee2);
       }));
 
       function displayAddOnCards(_x) {
