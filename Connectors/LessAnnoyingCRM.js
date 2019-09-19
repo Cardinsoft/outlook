b8fe97e73fa122a733cf17346afa72fede0bf8cd
Oleg Valter <oleg@cardinsoft.com>
2019-09-19 04:05:35 +0300

    Update LessAnnoyingCRM.js

--- Connectors/LessAnnoyingCRM.js
+++ Connectors/LessAnnoyingCRM.js
@@ -36,16 +36,6 @@ function LessAnnoyingCRM() {
       content: '',
       hint: 'e.g. 3587196306',
       multiline: true
-    }, {
-      type: globalKeyValue,
-      title: 'User list',
-      content: 'If you want to be able to assign contacts to different users than yourself, please, provide their ids (comma-separated)'
-    }, {
-      type: globalTextInput,
-      title: 'User Ids',
-      content: '',
-      hint: 'e.g. 123456',
-      name: 'userIds'
     }]
   }];
   this.auth = {
