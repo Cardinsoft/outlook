4ab1e6ff4327c5072f1e0fd4d0b7015549b61071
Oleg Valter <oleg@cardinsoft.com>
2019-09-19 08:37:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -168,17 +168,16 @@ Menu.prototype.addItem = function (item, isCardAction) {
     return regeneratorRuntime.wrap(function _callee2$(_context2) {
       while (1) switch (_context2.prev = _context2.next) {
         case 0:
-          self.switchShow(); //initialize, display Cards and build;
+          self.switchShow();
+          console.log(this); //initialize, display Cards and build;
 
-          response = new ActionResponseBuilder();
-          _context2.next = 4;
-          return response.build();
+          response = new UniversalActionResponseBuilder(); //await displayAddOnCards();
 
-        case 4:
+        case 3:
         case "end":
           return _context2.stop();
       }
-    }, _callee2);
+    }, _callee2, this);
   })));
   return this;
 };
