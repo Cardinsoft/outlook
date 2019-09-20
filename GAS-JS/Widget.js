9bdb991d1d4bdf755bfc78b2faa153ed15569f13
Oleg Valter <oleg@cardinsoft.com>
2019-09-20 07:20:00 +0300

    Update Widget.js

--- GAS-JS/Widget.js
+++ GAS-JS/Widget.js
@@ -1,57 +1,16 @@
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) {
-  if (call && (typeof call === "object" || typeof call === "function")) {
-    return call;
-  }
+function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-  return _assertThisInitialized(self);
-}
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-  }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
-  return self;
-}
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
-function _getPrototypeOf(o) {
-  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
-    return o.__proto__ || Object.getPrototypeOf(o);
-  };
-  return _getPrototypeOf(o);
-}
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function");
-  }
-
-  subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: {
-      value: subClass,
-      writable: true,
-      configurable: true
-    }
-  });
-  if (superClass) _setPrototypeOf(subClass, superClass);
-}
-
-function _setPrototypeOf(o, p) {
-  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
-    o.__proto__ = p;
-    return o;
-  };
-
-  return _setPrototypeOf(o, p);
-} //Emulate base Class Widget for CardService service;
-
-
+//Emulate base Class Widget for CardService service;
 function Widget() {
   this.className = 'Widget';
 }
@@ -77,16 +36,16 @@ TextParagraph.prototype.appendToUi = function (parent) {
   //append row;
   const widget = document.createElement('div');
   widget.className = 'row ' + this.className;
-  parent.appendChild(widget); //append column;
+  parent.append(widget); //append column;
 
   const wrapText = document.createElement('div');
   wrapText.className = 'column-text';
-  widget.appendChild(wrapText); //set widget text;
+  widget.append(wrapText); //set widget text;
 
   const content = document.createElement('span');
   content.className = 'ms-font-m-plus';
   content.innerHTML = this.content;
-  wrapText.appendChild(content);
+  wrapText.append(content);
 }; //Emulate Class ButtonSet extending base Class Widget for CardService service;
 
 
@@ -120,10 +79,10 @@ ButtonSet.prototype.appendToUi = function (parent) {
   const length = buttons.length;
   const btnRow = document.createElement('div');
   btnRow.className = 'row ' + this.className;
-  parent.appendChild(btnRow);
+  parent.append(btnRow);
   const wrapBtn = document.createElement('div');
   wrapBtn.className = 'column';
-  btnRow.appendChild(wrapBtn);
+  btnRow.append(wrapBtn);
   buttons.forEach(function (button) {
     const backgroundColor = button.backgroundColor;
     const text = button.text;
@@ -132,6 +91,6 @@ ButtonSet.prototype.appendToUi = function (parent) {
     const action = button.action;
     const openLink = button.openLink;
     const authAction = button.authorizationAction;
-    button.appendToUi(wrapBtn);
+    button.appendToUi(wrapBtn, set);
   });
 };
