ad26cefaab6b83b2fb5c4fe29fc37b0243987637
Oleg Valter <oleg@cardinsoft.com>
2019-09-25 08:22:57 +0300

    Update Button.js

--- GAS-JS/Button.js
+++ GAS-JS/Button.js
@@ -170,7 +170,7 @@ TextButton.prototype.appendToUi = function (parent, isSet) {
   button.className = this.className;
   button.type = 'button';
   button.disabled = disabled;
-  button.textContent = text; //access button style and class list;
+  button.innerText = text; //access button style and class list;
 
   const st = button.style;
   const cl = button.classList;
